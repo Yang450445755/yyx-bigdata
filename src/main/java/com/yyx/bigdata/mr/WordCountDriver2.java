c6285567612eb3ec9c8ae6f8fa8906f40bae3524
Yang450445755 <450445755@qq.com>
2020-07-29 19:27:26 +0800

    333

--- src/main/java/com/yyx/bigdata/mr/WordCountDriver2.java
+++ src/main/java/com/yyx/bigdata/mr/WordCountDriver2.java
@@ -1,5 +1,6 @@
 package com.yyx.bigdata.mr;
 
+import com.yyx.bigdata.util.FileUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
---
+++ src/main/scala/com/yyx/bigdata/ruoze/IfApp.scala
@@ -0,0 +1,29 @@
+package com.yyx.bigdata.ruoze
+
+/**
+ * @author PK哥
+ **/
+object IfApp {
+
+  def main(args: Array[String]): Unit = {
+
+    val a = 11
+    if(a % 2 ==0){
+      println(s"$a 是偶数")
+    }
+
+    if(a % 2 ==0){
+      println(s"$a 是偶数")
+    } else {
+      println(s"$a 是奇数")
+    }
+
+    val x = 10
+    val y = 20
+    val max = if(x > y) x else y
+    println(max)
+
+
+  }
+
+}
